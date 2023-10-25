1a15a92799e2be165f761865cc423804787362f8
Zhu-Joseph <josephzhu3@gmail.com>
2023-10-24 19:53:30 -0700

    add keyboard inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
 
 function App() {
   const [solution, setSolution] = useState(null)
+
   useEffect(() => {
     fetch('http://localhost:3001/solutions')
       .then(res => res.json())
@@ -14,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
-      {solution && <div>Solution is: {solution}</div>}
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 }
